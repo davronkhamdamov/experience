ae4b69d3b9abffe8381c13e69351c39c3b0fe4ae
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-02-16 01:27:04 +0500

    added nextjs

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>HTML blog</span>,
+  project: {
+    link: 'https://github.com/davronkhamdamov/html-blog.git',
+  },
+  docsRepositoryBase: 'https://github.com/davronkhamdamov/html-blog.git',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
